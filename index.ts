cab98caa9b9a1db31104e5ee34fea641794724aa
hammmad-03444 <160322774+hammmad-03444@users.noreply.github.com>
2024-03-30 13:16:23 +0500

    created cli guessing game

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import   inquirer from "inquirer";
+let randomNum=Math.floor( Math.random()*6+1)
+const userguess=await inquirer.prompt([
+    {name:"usernumber",
+    type:"number",
+    message:"Enter you number here"
+}
+])
+if(userguess.usernumber===randomNum){
+    console.log('Congratrulations!! You won, you guessed the right number')
+}
+else{
+    console.log('Bad luck!! better luck next time.')
+}
